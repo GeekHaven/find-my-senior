ee935f90c35d464bee9a7b0e05e5d5e788c9035a
Vrishabh Agamya <agamya.vrishabh@gmail.com>
2020-10-14 12:21:48 +0530

    Added AppBar and Drawer in dashboard

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:find_my_senior/root.dart';
 import 'package:find_my_senior/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:find_my_senior/services/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,27 +10,48 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final AuthService _auth = AuthService();
+  String userName;
+  getUserName() async {
+    userName = await SharedPreferencesUtil.getUserName();
+    setState(() {
+      print(userName);
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    getUserName();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Find My Senior'),
+      ),
       drawer: Drawer(
         child: ListView(
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
-              child: Text('Find Senior'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
+            UserAccountsDrawerHeader(
+              accountEmail: null,
+              accountName: Text(
+                userName,
+                style: TextStyle(fontSize: 20.0),
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  userName.substring(0, 1),
+                  style: TextStyle(fontSize: 30.0),
+                ),
               ),
             ),
             ListTile(
               title: Text('Edit Profile'),
+              trailing: Icon(Icons.account_circle),
               onTap: () {
                 // Update the state of the app.
                 // ...
@@ -37,6 +59,7 @@ class _HomePageState extends State<HomePage> {
             ),
             ListTile(
               title: Text('Log Out'),
+              trailing: Icon(Icons.exit_to_app),
               onTap: () {
                 // Update the state of the app.
                 // ...
