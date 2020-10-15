c60ee1108b65f531ea778da4c8b86eeca3cc63a9
Anushka Verma <anushkavermatheprincess@gmail.com>
2020-10-16 03:40:34 +0530

    search feature added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:find_my_senior/root.dart';
 import 'package:find_my_senior/services/auth_service.dart';
+import 'package:find_my_senior/services/searchservice.dart';
 import 'package:flutter/material.dart';
 import 'package:find_my_senior/services/shared_preferences.dart';
 
@@ -9,9 +11,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String name;
+  bool searchState = false;
+  var queryResultSet = [];
+  var tempSearchStore = [];
   final AuthService _auth = AuthService();
   String userName;
+  intiateSearch(value) {
+    if (value.length == 0) {
+      setState(() {
+        queryResultSet = [];
+        tempSearchStore = [];
+      });
+    }
+    var capitalizedValue =
+        value.substring(0, 1).toUpperCase() + value.substring(1);
+
+    if (queryResultSet.length == 0 && value.length == 1) {
+      SearchService().searchByName(value).then((QuerySnapshot docs) {
+        for (int i = 0; i < docs.documents.length; ++i) {
+          queryResultSet.add(docs.documents[i].data);
+        }
+        setState(() {
+          tempSearchStore = queryResultSet;
+        });
+      });
+    } else {
+      tempSearchStore = [];
+      queryResultSet.forEach((element) {
+        if (element['name'].startsWith(capitalizedValue)) {
+          setState(() {
+            tempSearchStore.add(element);
+          });
+        }
+      });
+    }
+  }
+
   getUserName() async {
     userName = await SharedPreferencesUtil.getUserName();
     setState(() {
@@ -29,7 +64,38 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Find My Senior'),
+        title: !searchState
+            ? Text('Find My Senior')
+            : TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.search, color: Colors.black),
+                  hintText: 'Search...',
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+                onChanged: (val) {
+                  intiateSearch(val);
+                },
+              ),
+        actions: <Widget>[
+          !searchState
+              ? IconButton(
+                  icon: Icon(Icons.search, color: Colors.white),
+                  onPressed: () {
+                    setState(() {
+                      searchState = !searchState;
+                    });
+                  },
+                )
+              : IconButton(
+                  icon: Icon(Icons.cancel, color: Colors.white),
+                  onPressed: () {
+                    setState(() {
+                      searchState = !searchState;
+                    });
+                  },
+                ),
+        ],
       ),
       drawer: Drawer(
         child: ListView(
@@ -69,28 +135,37 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextFormField(
-              style: TextStyle(color: Colors.black),
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
-                    ),
-                  ),
-                  hintText: 'Search...',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  icon: Icon(Icons.search)),
-              onChanged: (value) => name = value,
-            ),
-          ),
-        ],
-      ),
+      body: ListView(children: [
+        SizedBox(
+          height: 20.0,
+        ),
+        GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+            primary: false,
+            shrinkWrap: true,
+            children: tempSearchStore.map((element) {
+              return buildResultCard(element);
+            }).toList())
+      ]),
     );
   }
+}
+
+Widget buildResultCard(data) {
+  return Card(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+    elevation: 2.0,
+    child: Container(
+      child: Text(
+        data['name'],
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20.0,
+        ),
+      ),
+    ),
+  );
 }
